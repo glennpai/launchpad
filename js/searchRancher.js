841df619dae13062ff67e444737f9e8a3f777f43
glennc <glennc@ohio.edu>
2020-07-14 21:16:08 -0400

    Genesis

---
+++ js/searchRancher.js
@@ -0,0 +1,9 @@
+function searchRancher(){
+    var app = document.getElementById('rancherApp').value;
+    var type = document.getElementById('rancherType').value;
+    var env = document.getElementById('rancherEnv').value;
+
+    var url = ''
+
+    window.open("https://www.google.com")
+}
